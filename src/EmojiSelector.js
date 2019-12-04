1ffb73fcc703cf0b5127dc3426d72017f804ee0e
Zach Sherman <zach@anemon.es>
2019-12-03 17:40:31 -0800

    destructure arguments in fn header

--- src/EmojiSelector.js
+++ src/EmojiSelector.js
@@ -1,25 +1,18 @@
 import React from 'react'
 import './EmojiSelector.css'
 
-export default function(props) {
-  const { updatePreview, name, description } = props
-  return (
-    <div className='emoji-selector'>
-      <button
-        name={name + 'Good'}
-        onClick={updatePreview}
-        aria-label='thumbs-up'
-      >
-        👍
-      </button>
-      <p>{description}</p>
-      <button
-        name={name + 'Bad'}
-        onClick={updatePreview}
-        aria-label='thumbs-down'
-      >
-        👎
-      </button>
-    </div>
-  )
-}
+export default ({ updatePreview, name, description }) => (
+  <div className='emoji-selector'>
+    <button name={name + 'Good'} onClick={updatePreview} aria-label='thumbs-up'>
+      👍
+    </button>
+    <p>{description}</p>
+    <button
+      name={name + 'Bad'}
+      onClick={updatePreview}
+      aria-label='thumbs-down'
+    >
+      👎
+    </button>
+  </div>
+)
--- src/Form.js
+++ src/Form.js
@@ -2,49 +2,44 @@ import React from 'react'
 import EmojiSelector from './EmojiSelector'
 import './Form.css'
 
-function Form(props) {
-  const updatePreview = props.updatePreview
-  return (
-    <div className='form'>
-      <label className='form-label'>Your feedback</label>
-      <textarea
-        className='form-textarea'
-        name='feedback'
-        rows='4'
-        onChange={updatePreview}
-        placeholder='Let us know what we did well or could improve...'
-      />
+export default ({ updatePreview }) => (
+  <div className='form'>
+    <label className='form-label'>Your feedback</label>
+    <textarea
+      className='form-textarea'
+      name='feedback'
+      rows='4'
+      onChange={updatePreview}
+      placeholder='Let us know what we did well or could improve...'
+    />
 
-      <label className='form-label'>How many in your party?</label>
-      <input
-        className='form-input'
-        type='number'
-        name='party'
-        min='1'
-        onChange={updatePreview}
-        placeholder='2'
-      />
+    <label className='form-label'>How many in your party?</label>
+    <input
+      className='form-input'
+      type='number'
+      name='party'
+      min='1'
+      onChange={updatePreview}
+      placeholder='2'
+    />
 
-      <label className='form-label'>How was your meal?</label>
-      <div className='emoji-form'>
-        <EmojiSelector
-          updatePreview={updatePreview}
-          name='food'
-          description='How tasty was the food?'
-        />
-        <EmojiSelector
-          updatePreview={updatePreview}
-          name='service'
-          description='How was our service?'
-        />
-        <EmojiSelector
-          updatePreview={updatePreview}
-          name='ambience'
-          description='Did you like the ~ambience~ ?'
-        />
-      </div>
+    <label className='form-label'>How was your meal?</label>
+    <div className='emoji-form'>
+      <EmojiSelector
+        updatePreview={updatePreview}
+        name='food'
+        description='How tasty was the food?'
+      />
+      <EmojiSelector
+        updatePreview={updatePreview}
+        name='service'
+        description='How was our service?'
+      />
+      <EmojiSelector
+        updatePreview={updatePreview}
+        name='ambience'
+        description='Did you like the ~ambience~ ?'
+      />
     </div>
-  )
-}
-
-export default Form
+  </div>
+)
